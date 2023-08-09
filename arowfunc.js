a6fce99a4da18ce83a701fffb1bfcd53490362ee
Nigam Sharma <132887723+THARMATT@users.noreply.github.com>
2023-08-09 10:27:16 +0530

    Create arowfunc.js

---
+++ arowfunc.js
@@ -0,0 +1,18 @@
+
+
+const hello=(name,greet)=>
+  console.log("hlo "+ name + greet);
+  
+const x={
+  name:"nigam",
+  role:"js developer",
+exp:"2 months",
+
+show:function(){
+  // let that=this
+  setTimeout(()=>{console.log(`${this.role}`)},7000)
+  
+}}
+// console.log(x.name,x.exp)
+// hello("kalloo","bhag lowde")
+x.show()
